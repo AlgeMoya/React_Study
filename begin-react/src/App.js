097ca3a41efd917511a9f04d882f7bca81ac1f65
AlgeMoya <kjhobd2@naver.com>
2022-12-13 13:37:54 +0900

    20221212

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo, useCallback } from 'react';
 import Hello from './hello';
 import './App.css';
 import Wrapper from './Wrapper';
@@ -9,6 +9,11 @@ import CreateUser from './CreateUser';
 // import logo from './logo.svg';
 // import './App.css';
 
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는 중...');
+  return users.filter(user => user.active).length;
+}
+
 function App() {
   const [inputs, setInputs] = useState({
     username: '',
@@ -17,41 +22,48 @@ function App() {
 
   const {username, email} = inputs;
   
-  const onChange = e => {
-    const {name, value} = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
+  const onChange = useCallback(
+    e => {
+      const {name, value} = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value
+      });
+    }, 
+  [inputs]
+  );
 
   const [users, setUsers] = useState([
     {
         id: 1, 
         username: 'velopert', 
-        email: 'public.velopert@gmail.com'
+        email: 'public.velopert@gmail.com',
+        active: true
     }, 
     {
         id: 2, 
         username: 'tester', 
-        email: 'tester@example.com'
+        email: 'tester@example.com',
+        active: true
     }, 
     {
         id: 3, 
         username: 'liz', 
-        email: 'liz@example.com'
+        email: 'liz@example.com',
+        active: true
     }
   ]);
   
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback;() => {
     // 배열에 항목 추가하는 로직
     const user = {
       id: nextId.current,
       username,
       email
     };
-    setUsers([...users, user]);
+    setUsers([...users, user]); // spread 연산자
+    // setUsers(users.concat(user)); // concat
 
     setInputs({
       username: '',
@@ -60,6 +72,26 @@ function App() {
     nextId.current += 1;
   }
   
+  const onRemove = useCallback(
+    id => {
+      // user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듦
+      // user.id가 id인 것을 제거함
+      setUsers(users.filter(user => user.id !== id)); // users에서 user.id가 id가 아닌 것들만 남긴 새로운 배열로 대체한다.
+    }, [users]
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setUsers(users.map(user => 
+        user.id === id ? {...user, active: !user.active} : user
+        )
+      );
+    }, [users]
+  );
+
+  // const count = countActiveUsers(users);
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   let name = 'react';
 
   let style = {
@@ -94,7 +126,8 @@ function App() {
           onChange={onChange}
           onCreate={onCreate}
         />
-        <UserList users={users} />
+        <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+        <div>활성사용자 수 : {count}</div>
       </>
     </Wrapper>
 
